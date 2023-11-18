de7927472cf269139914e32f1bf266aae372fd63
efe-get-protocol <111286837+efe-get-protocol@users.noreply.github.com>
2023-11-18 14:03:36 +0100

    smart posts workspace added

--- marketing/src/pages/api/bigquery.ts
+++ marketing/src/pages/api/bigquery.ts
@@ -7,19 +7,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     console.log("heree??")
   const optionsQuery = {
       keyFilename: 'keyfile.json',
-      projectId: 'bigdaddylens',
     } as BigQueryOptions;
   const client = new BigQuery(optionsQuery)
-  console.log("client", client)
   const query = `
-  SELECT * 
-  FROM lens-public-data.v2_polygon.profile_interest
-  WHERE profile_id IN (
-      SELECT profile_follower_id 
-      FROM lens-public-data.v2_polygon.profile_follower
-      WHERE profile_id = '0x28a2'
-      LIMIT 3
-  );
+  select table_name from lens-public-data.v2_polygon.INFORMATION_SCHEMA.TABLES
   `
 
   const options = {
